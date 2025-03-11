e08ed12c9f8d3dd22707ee5a6b7623099b54c166
GVK <venkatakousikcse01@gmail.com>
2025-03-11 12:12:41 +0530

    feat: added new changes

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../images/aditya.jpg';
+import Logo from '../images/aditya.png';
 import { useState } from 'react';
 import { FaSortDown } from "react-icons/fa";
 
